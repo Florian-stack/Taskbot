9fcc4c4906af083efbcb6d78d99ebed5b7672d83
Florian-stack <florian.stipp@gmail.com>
2025-03-25 09:59:49 +0000

    added bot.js and config.js and obfusicated bot.js

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    token: 'DEIN_BOT_TOKEN',
+    clientId: 'DEINE_CLIENT_ID',
+    guildId: 'DEINE_GUILD_ID',
+    logChannelId: 'DEIN_LOG_CHANNEL_ID',
+    dashboardChannelId: 'DEIN_DASHBOARD_CHANNEL_ID',
+    allowedRoleIds: ['ROLLE_ID_1', 'ROLLE_ID_2']
+  };
